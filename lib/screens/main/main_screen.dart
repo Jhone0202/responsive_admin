1f1ef087507c2e2ea2a41e69dc79927913c3fd89
Jhone0202 <maicon.jhone100@gmail.com>
2021-05-05 00:19:20 -0300

    <Change log> 05/05/2021

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'components/side_menu.dart';
+
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: SideMenu(),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
